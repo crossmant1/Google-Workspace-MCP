e999256cc308c71af8007b6c768d75b1de089fd7
Thomas Crossman <crossmant1@udayton.edu>
2025-10-17 10:26:29 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
-from fastmcp import fastMCP
+from fastmcp import FastMCP
 from dotenv import load_dotenv
 import os, requests, json
 
